8c4d8b6a72913fd31786e8257bfb1a3bb2c037f7
hampusvh <hampus.vh@gmail.com>
2025-05-12 17:41:52 +0200

    minor changes in pages/components

--- övningsuppgift-1/jwt-auth/frontend/src/App.jsx
+++ övningsuppgift-1/jwt-auth/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
@@ -10,15 +11,12 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        {/* Publika sidor */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Skyddade sidor */}
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
